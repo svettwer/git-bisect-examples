f0e37e1f54651abd9417f3e16864879003f9d781
Sven Hettwer <sven.hettwer@consol.de>
2018-01-08 14:01:17 +0100

    Added Test for extraction of standard cake generation into a cake service

---
+++ cake-factory/src/test/java/com/github/example/cakefactory/services/CakeServiceTest.java
@@ -0,0 +1,31 @@
+package com.github.example.cakefactory.services;
+
+import com.github.example.cakefactory.dto.Cake;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CakeServiceTest {
+
+    @Resource
+    private CakeService cakeService;
+
+    @Test
+    public void defaultCakeIsChocolate() {
+
+        //GIVEN
+
+        //WHEN
+        Cake cake = cakeService.getCake();
+
+        //THEN
+        assertEquals("chocolate", cake.getKind());
+    }
+}
