9a22c03327278eb9974d1d3c67b2d51f678ee008
Sven Hettwer <sven.hettwer@consol.de>
2018-01-08 14:02:33 +0100

    Use cakeService in cakeController instead of generating cake directly

--- cake-factory/src/main/java/com/github/example/cakefactory/controller/CakeController.java
+++ cake-factory/src/main/java/com/github/example/cakefactory/controller/CakeController.java
@@ -1,16 +1,22 @@
 package com.github.example.cakefactory.controller;
 
 import com.github.example.cakefactory.dto.Cake;
+import com.github.example.cakefactory.services.CakeService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping(path = "/cake")
 public class CakeController {
 
+    @Resource
+    private CakeService cakeService;
+
     @RequestMapping(method = RequestMethod.GET)
     public Cake getCake(){
-        return new Cake("chocolate");
+        return cakeService.getCake();
     }
 }
--- cake-factory/src/test/java/com/github/example/cakefactory/controller/CakeControllerTest.java
+++ cake-factory/src/test/java/com/github/example/cakefactory/controller/CakeControllerTest.java
@@ -1,13 +1,18 @@
 package com.github.example.cakefactory.controller;
 
 import com.github.example.cakefactory.dto.Cake;
+import com.github.example.cakefactory.services.CakeService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -16,15 +21,20 @@ public class CakeControllerTest {
     @Resource
     private CakeController cakeController;
 
+    @MockBean
+    private CakeService cakeService;
+
     @Test
     public void getCake() {
 
         //GIVEN
+        Cake expectedCake = mock(Cake.class);
+        when(cakeService.getCake()).thenReturn(expectedCake);
 
         //WHEN
         Cake cake = cakeController.getCake();
 
         //THEN
-        Assert.assertEquals("chocolate", cake.getKind());
+        Assert.assertEquals(expectedCake, cake);
     }
 }
